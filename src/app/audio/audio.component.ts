d94f5c7451c151b60f8d868f4b777d3652ffa0cb
Navid Boloorian <navidboloorian0@gmail.com>
2024-11-21 15:25:10 -0800

    added event markers

--- src/app/audio/audio.component.ts
+++ src/app/audio/audio.component.ts
@@ -1,11 +1,13 @@
 import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import { AudioService } from '../audio.service';
 import { SecondsToTimestampPipe } from '../pipes/seconds-to-timestamp.pipe';
+import { DataService } from '../data.service';
+import { TimestampToSecondsPipe } from '../pipes/timestamp-to-seconds.pipe';
 
 @Component({
   selector: 'dv-audio',
   standalone: true,
-  imports: [SecondsToTimestampPipe],
+  imports: [SecondsToTimestampPipe, TimestampToSecondsPipe],
   templateUrl: './audio.component.html',
   styleUrl: './audio.component.scss',
 })
@@ -14,8 +16,13 @@ export class AudioComponent implements AfterViewInit {
   @ViewChild('slider') sliderElement!: ElementRef<HTMLInputElement>;
   @ViewChild('play') play!: ElementRef<HTMLButtonElement>;
   @ViewChild('pause') pause!: ElementRef<HTMLButtonElement>;
+  @ViewChild('markers') markers!: ElementRef<HTMLDivElement>;
 
-  constructor(public audioService: AudioService) {}
+  constructor(
+    public audioService: AudioService,
+    public dataService: DataService,
+    private _timestampToSecondsPipe: TimestampToSecondsPipe,
+  ) {}
 
   ngAfterViewInit(): void {
     this.audioService.bindElements(this.audioElement, this.sliderElement);
@@ -27,8 +34,40 @@ export class AudioComponent implements AfterViewInit {
     this.pause.nativeElement.onclick = () => {
       this.audioElement.nativeElement.pause();
     };
+
+    this.audioElement.nativeElement.ondurationchange = () => {
+      this.createMarkers();
+    };
   }
 
-  protected readonly console = console;
-  protected readonly NaN = NaN;
+  createMarkers(): void {
+    if (!this.audioElement.nativeElement.duration) return;
+
+    const nativeElement = this.markers.nativeElement;
+    const totalSeconds = Math.floor(this.audioElement.nativeElement.duration);
+
+    console.log(this.audioElement.nativeElement.duration);
+
+    for (const datum of this.dataService.data) {
+      if (datum.timestamp) {
+        const marker = document.createElement('div');
+
+        const markerSeconds = this._timestampToSecondsPipe.transform(
+          datum.timestamp,
+        );
+
+        console.log(markerSeconds);
+
+        const leftPosition = (markerSeconds / totalSeconds) * 100;
+
+        marker.style.position = 'absolute';
+        marker.style.left = `${leftPosition}%`;
+        marker.style.background = 'red';
+        marker.style.width = '2px';
+        marker.style.height = '100%';
+
+        nativeElement.appendChild(marker);
+      }
+    }
+  }
 }
